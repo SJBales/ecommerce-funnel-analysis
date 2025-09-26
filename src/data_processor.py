3ef37d61e9e8812eebdbb3b24c14fcbe20a02b75
balessj <sbales556@gmail.com>
2025-09-25 21:00:04 -0500

    Changed name of data loader to data processor. Removed unnecessary queries to prep for the full pipeline.

--- src/data_processor.py
+++ src/data_processor.py
@@ -9,12 +9,3 @@ FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.INFORMATION_SCHEMA.TA
 ORDER BY table_name
 """
 tables = client.query(query).to_dataframe()
-print(tables)
-
-query2 = """
-SELECT *
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101`
-LIMIT 10"""
-
-columns = client.query(query2).to_dataframe()
-print(columns)
