37675ffe98847d988de89f730018cdceb5a269e3
balessj <sbales556@gmail.com>
2025-10-04 09:32:28 -0500

    Added methods in data viz and processing for:     - Aggregate events over time     - Conversion rates over time

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,7 +1,12 @@
 from data_loader import ecommerce_loader
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-# Setting up the class
+# Setting up the data processor class
 class ecommerce_processor:
     def __init__(self) -> None:
         self.event_df = None
@@ -9,18 +14,49 @@ class ecommerce_processor:
         self.device_df = None
         self.geo_df = None
 
+    # Method for running the queries and storing the results
     def run_queries(self) -> None:
         self.event_df,
         self.session_df,
         self.device_df,
         self.geo_df = ecommerce_loader()
 
+    # Method to pivot the event data from wide to long
     def prep_events(self) -> None:
-        self.long_event_df = self.event_results.melt(
+        self.long_event_df = self.event_df.melt(
             id_vars=['user_pseudo_id',
                      'first_event_date',
                      'first_event_timestamp'])\
             .rename(columns={'variable': 'event', 'value': 'occurence'})
+        self._long_event_initialized = True
+
+        logger.INFO("Converted events from wide to long")
+
+    # Method to pivot the session conversion data from wide to long
+    def prep_session(self) -> None:
+        self.long_session_df = self.session_df.melt(
+            id_vars=['user_pseudo_id',
+                     'session',
+                     'first_event_date',
+                     'first_event_timestamp'])\
+            .rename(columns={'variable': 'event', 'value': 'occurence'})
+
+        logger.INFO("Converted session from wide to long")
+
+    # Method to get aggregate conversions by event
+    def prep_agg_conversion(self) -> None:
+
+        # Checking if events have been prepped
+        if not self._long_event_initialized:
+            self.prep_events()
+        else:
+            self.agg_long_events_df = self.long_event_df\
+                .loc[:, ['first_event_date',
+                         'event',
+                         'occurence']]\
+                .groupby(['first_event_date', 'event']).sum().reset_index()
+
+        logger.INFO("Aggregated events")
 
 
 if __name__ == "__main__":
--- src/visualization_engine.py
+++ src/visualization_engine.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -6,25 +7,84 @@ class ecommerce_viz:
     def __init__(self, processor):
         self.processor = processor
 
-    def plot_conversion(self, remove_pageview=False):
+    '''Section for helper functions'''
+    # Helper function for removing the pageview event
+    def remove_pageview_(self, df, remove_pageview=False) -> np.array:
 
         if remove_pageview:
-
-            row_mask = self.processor.long_events_df['event'] != 'viewed_page'
-
-            sns.barplot(x='event',
-                        y='occurence',
-                        data=self.processor.long_events_df[row_mask])
-            plt.title('Event Conversion Rates')
-            plt.ylabel('Conversion Rate')
-            plt.xlabel('Event')
-            plt.show()
-
+            mask = df['event'] != 'viewed_page'
         else:
-            sns.barplot(x='event',
-                        y='occurence',
-                        data=self.processor.long_events_df)
-            plt.title('Event Conversion Rates')
-            plt.ylabel('Conversion Rate')
-            plt.xlabel('Event')
-            plt.show()
+            mask = True
+
+        return mask
+
+    '''Section for overall event conversion'''
+    # Method to plot conversion rates
+    def plot_conversion_rate(self, remove_pageview=False) -> None:
+
+        # Calling the helper method for removing the pageview event
+        row_mask = self.remove_pageview_(self.processor.long_events_df,
+                                         remove_pageview)
+
+        # Creating plots
+        sns.set_theme()
+        sns.barplot(x='event',
+                    y='occurence',
+                    data=self.processor.long_events_df[row_mask])
+        plt.title('Event Conversion Rates')
+        plt.ylabel('Conversion Rate')
+        plt.xlabel('Event')
+        plt.show()
+
+    # Method for plotting conversion over time
+    def plot_conversion_rates_over_time(self, remove_pageview=False) -> None:
+
+        # Calling the helper method for removing the pageview event
+        row_mask = self.remove_pageview_(self.processor.long_events,
+                                         remove_pageview)
+
+        # Using the helper method
+        sns.set_theme()
+        sns.lineplot(data=self.processor.long_events[row_mask],
+                     x='first_event_date',
+                     y='occurence',
+                     hue='event')
+        plt.title('Aggregate Event Conversion Over Time')
+        plt.ylabel('Conversion Rate')
+        plt.xlabel('Event Date')
+        plt.show()
+
+    # Method to plot aggregate conversion rates over time
+    def plot_conversion_events_over_time(self, remove_pageview=False) -> None:
+
+        # Calling the helper method for removing the pageview event
+        row_mask = self.remove_pageview_(self.processor.agg_long_events_df,
+                                         remove_pageview)
+
+        sns.set_theme()
+        sns.lineplot(data=self.processor.agg_long_events_df[row_mask],
+                     x='first_event_date',
+                     y='occurence',
+                     hue='event')
+        plt.title('Aggregate Event Conversion Over Time')
+        plt.ylabel('Conversion Rate')
+        plt.xlabel('Event Date')
+        plt.show()
+
+    '''Section for session conversion plots'''
+    # Method to plot session conversion
+    def plot_session_conversion_rate(self, remove_pageview=False):
+
+        # Calling the helper method for removing the pageview event
+        row_mask = self.remove_pageview_(self.processor.long_session_df,
+                                         remove_pageview)
+
+        # Creating plots
+        sns.set_them()
+        sns.barplot(x='event',
+                    y='occurence',
+                    data=self.processor.long_session_df[row_mask])
+        plt.title('Session Conversion Rates')
+        plt.ylabel('Conversion Rate')
+        plt.xlabel('Event')
+        plt.show()
