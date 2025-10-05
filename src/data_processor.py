a988271d2eddd470ea9b785e614f8c5bfedfcbc3
balessj <sbales556@gmail.com>
2025-10-05 10:23:32 -0500

    Updates to the data processor class and methods - Added explicit setup of attributes in init - Fixed logic for testing if long events had been created - Updated docstrings

--- src/data_processor.py
+++ src/data_processor.py
@@ -7,12 +7,16 @@ logger = logging.getLogger(__name__)
 
 
 # Setting up the data processor class
-class ecommerce_processor:
+class ecommerceProcessor:
     def __init__(self) -> None:
         self.event_df = None
         self.session_df = None
         self.device_df = None
         self.geo_df = None
+        self.long_event_df = None
+        self.long_session_df = None
+        self.agg_long_events_df = None
+        self._long_event_initialized = False
 
     # Method for running the queries and storing the results
     def run_queries(self) -> None:
@@ -20,42 +24,49 @@ class ecommerce_processor:
 
     # Method to pivot the event data from wide to long
     def prep_events(self) -> None:
-        self.long_event_df = self.event_df.melt(
+        '''Converts the events dataframe from wide to long for vis'''
+
+        self.long_event_df = self.event_df.copy().melt(
             id_vars=['user_pseudo_id',
                      'first_event_date',
                      'first_event_timestamp'])\
             .rename(columns={'variable': 'event', 'value': 'occurence'})
         self._long_event_initialized = True
 
-        logger.INFO("Converted events from wide to long")
+        logger.info("Converted events from wide to long")
 
     # Method to pivot the session conversion data from wide to long
     def prep_session(self) -> None:
-        self.long_session_df = self.session_df.melt(
+        '''Converts the session dataframe from wide to long for vis'''
+
+        self.long_session_df = self.session_df.copy().melt(
             id_vars=['user_pseudo_id',
                      'session',
                      'first_event_date',
                      'first_event_timestamp'])\
             .rename(columns={'variable': 'event', 'value': 'occurence'})
 
-        logger.INFO("Converted session from wide to long")
+        logger.info("Converted session from wide to long")
 
     # Method to get aggregate conversions by event
     def prep_agg_conversion(self) -> None:
+        '''Gets aggregate event metrics for visualizations of total traffic'''
 
         # Checking if events have been prepped
-        if not self._long_event_initialized:
+        if self._long_event_initialized:
             self.prep_events()
-        else:
-            self.agg_long_events_df = self.long_event_df\
-                .loc[:, ['first_event_date',
-                         'event',
-                         'occurence']]\
-                .groupby(['first_event_date', 'event']).sum().reset_index()
 
-        logger.INFO("Aggregated events")
+        self.agg_long_events_df = self.long_event_df\
+            .loc[:, ['first_event_date',
+                     'event',
+                     'occurence']]\
+            .groupby(['first_event_date', 'event']).sum().reset_index()
+
+        logger.info("Aggregated events")
 
 
 if __name__ == "__main__":
-    processor = ecommerce_processor()
+    processor = ecommerceProcessor()
     processor.run_queries()
+    processor.prep_events()
+    print(processor.long_event_df.head())
